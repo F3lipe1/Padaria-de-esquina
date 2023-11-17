580f494012903c23d4de59394b9d499da0017060
yasminn93 <149119531+F3lipe1@users.noreply.github.com>
2023-11-16 21:16:15 -0300

    Padaria de Esquina
    Engajamos no primeiro contato ao PHP

---
+++ img/teste.php
@@ -0,0 +1,20 @@
+<?php 
+
+$nome = "Felipe Augusto";
+$cidade = "São Paulo";
+$telefone = "(11) 4002-8922";
+$email = "felipe123.gmail";
+$cep = "1234567";
+
+echo "Me chamo $nome moro em $cidade, telefone é $telefone e meu Email é $email. Meu Cep é $cep"
+
+
+
+
+
+
+
+
+
+
+?>
